78b6c0f8ade2d81b1d6ba45da83ea65b38e0a45e
Jose Catala <jfca68@gmail.com>
2024-12-06 13:36:58 +0300

    Fixed a crash with leaderboard

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -1,32 +1,30 @@
 // src/providers/Web3Provider.tsx
-import React from 'react';
 import { WagmiProvider, createConfig } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { gameChain } from '@/src/config/web3Config';
-import { http } from 'viem';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { mainnet, bscTestnet } from 'wagmi/chains';
 
-const config = createConfig({
- chains: [gameChain],
- transports: {
-   [gameChain.id]: http()
- },
+const config = getDefaultConfig({
+  appName: 'Shooting Game',
+  projectId: 'onedayvpn.com', // You need to add your WalletConnect project ID here
+  chains: [bscTestnet],
+  ssr: true,
 });
 
 const queryClient = new QueryClient();
 
 interface Props {
- children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function Web3Provider({ children }: Props) {
- return (
-   <WagmiProvider config={config}>
-     <QueryClientProvider client={queryClient}>
-       <RainbowKitProvider>
-         {children}
-       </RainbowKitProvider>
-     </QueryClientProvider>
-   </WagmiProvider>
- );
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
